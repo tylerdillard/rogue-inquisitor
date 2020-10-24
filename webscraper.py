6199b405adda700f8ae2e5c7583b880ec4903c4b
tyler <47397833+tylerdillard@users.noreply.github.com>
2020-10-24 12:17:31 -0400

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-base_url = 'https://thetrove.is/Books/Dungeons%20%26%20Dragons%20%5Bmulti%5D/5th%20Edition%20%285e%29/Core/'
-response = requests.get(base_url, timeout=5)
+folder = 'https://thetrove.is/Books/Dungeons%20%26%20Dragons%20%5Bmulti%5D/5th%20Edition%20%285e%29/Core/'
+response = requests.get(folder, timeout=5)
 soup = BeautifulSoup(response.content, 'html.parser')
 cells = soup.find_all('td', {'class': 'link'})
+base_url = folder.format('{}')
+print(base_url)
 
 for td in cells:
     links = (td.a['href'])
-    print(links)
+    download_url = (base_url+links)
+    print(download_url)
