1ff10705a775627e0738b52f3a93c7ba757ba39b
tyler <47397833+tylerdillard@users.noreply.github.com>
2020-10-24 13:38:00 -0400

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,13 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
-import urllib.parse
 
-folder = 'https://thetrove.is/Books/Dungeons%20%26%20Dragons%20%5Bmulti%5D/5th%20Edition%20%285e%29/Core/'
-response = requests.get(folder, timeout=5)
-soup = BeautifulSoup(response.content, 'html.parser')
-cells = soup.find_all('td', {'class': 'link'})
+def get_book_links()
+
+    archive_url = 'https://thetrove.is/Books/Dungeons%20%26%20Dragons%20%5Bmulti%5D/5th%20Edition%20%285e%29/Core/'
+    r = requests.get(folder, timeout=5)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    cells = soup.find_all('td', {'class': 'link'})
 
 for td in cells:
     books = (td.a['href'])
-    download_url = urllib.parse.urljoin(folder, books)
-    print(download_url)
+    download_url = urllib.parse.urljoin(archive_url, books)\
+    r = requests.get(download_url, stream = True)
+    
+    with open(books,'wb') as f:
+        f.write(r.content)
