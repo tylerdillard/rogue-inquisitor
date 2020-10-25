f873a71f7bb626e747bb4fec115ae88e05f338c5
tyler <47397833+tylerdillard@users.noreply.github.com>
2020-10-25 11:30:20 -0400

    first successful download of single folder

--- webscraper.py
+++ webscraper.py
@@ -1,14 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 import urllib
+import os
+from urllib.parse import urlparse
 
 archive_url = 'https://thetrove.is/Books/Dungeons%20%26%20Dragons%20%5Bmulti%5D/5th%20Edition%20%285e%29/Core/'
 r = requests.get(archive_url, timeout=5)
 soup = BeautifulSoup(r.content, 'html.parser')
-cells = soup.find_all('td', {'class': 'link'})
-    
-for td in cells:
-    books = (td.a['href'])
-    download_url = urllib.parse.urljoin(archive_url, books)
+cells = soup.find_all('td', {'class': 'link'})[1:]
 
-print(download_url)
+for td in cells:
+    book_path = (td.a['href'])
+    book_name = (td.text)
+    download_url = urllib.parse.urljoin(archive_url, book_path)
+    d = requests.get(download_url, stream=True)
+    
+    with open('C:/Users/Tyler/Documents/5e/Core/'+ book_name, 'wb') as f :
+        f.write(d.content)
+        print('downloading' + book_name)
+
+print('sleight of hand check successful')
