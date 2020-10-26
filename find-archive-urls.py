1afa69c9683f07194a6ce66db405fe6f4d8d24b2
tyler <47397833+tylerdillard@users.noreply.github.com>
2020-10-26 15:16:04 -0400

    Update find-archive-urls.py

--- find-archive-urls.py
+++ find-archive-urls.py
@@ -1,14 +1,6 @@
-from bs4 import BeautifulSoup
-import requests
-import urllib
-from urllib.parse import urlparse
 from investigate import investigate
-from downloadScroll import downloadScroll
 
 archive_url = 'https://thetrove.is/Books/Dungeons%20%26%20Dragons%20%5Bmulti%5D/5th%20Edition%20%285e%29/'
-r = requests.get(archive_url, timeout=5)
-soup = BeautifulSoup(r.content, 'html.parser')
-cells = soup.find_all('td', {'class': 'link'})[1:]
 
 investigate(archive_url)
 
