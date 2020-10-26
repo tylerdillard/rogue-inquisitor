4a64462931dc6eb4784544dbcd98a3e5c1d13549
tyler <47397833+tylerdillard@users.noreply.github.com>
2020-10-26 14:17:59 -0400

    update downloadScroll

--- downloadScroll.py
+++ downloadScroll.py
@@ -1,16 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
+import urllib
+from urllib.parse import urlparse
 from time import sleep
 import os
 
 def downloadScroll(download_url, scroll_name):
 
     print('downloading ' + scroll_name)
+    raw_directory = urlparse(download_url)
+    pretty_directory = urllib.parse.unquote_plus(raw_directory.path)
+    
+    print(raw_directory)
+    print(pretty_directory)
 
     #d = requests.get(download_url, stream=True)
     
-    #with open('C:/Users/Tyler/Documents/5e/Core/'+ scroll_name, 'wb') as f :
+    #with open('C:/Users/Tyler/Documents'+pretty_directory 'wb') as f :
         #f.write(d.content)
     
     #sleep(2)
-print('sleight of hand check successful')
+
+#print('sleight of hand check successful')
