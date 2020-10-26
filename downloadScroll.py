7add3863281cf97f1fb3c1b56dcfeabb63a5aea5
tyler <47397833+tylerdillard@users.noreply.github.com>
2020-10-26 15:13:58 -0400

    downloadScroll now creates directories

--- downloadScroll.py
+++ downloadScroll.py
@@ -10,15 +10,23 @@ def downloadScroll(download_url, scroll_name):
     print('downloading ' + scroll_name)
     raw_directory = urlparse(download_url)
     pretty_directory = urllib.parse.unquote_plus(raw_directory.path)
-    
-    print(raw_directory)
-    print(pretty_directory)
+    split_directory = pretty_directory.split('/')[2:-1]
+    split_filepath = pretty_directory.split('/')[2:]
+    joined_directory = '/'.join(split_directory)
+    joined_filepath = '/'.join(split_filepath)
+    save_directory = 'C:/Users/Tyler/Documents/'+joined_directory
+    save_filepath = 'C:/Users/Tyler/Documents/'+joined_filepath
 
+    #check_folder = os.path.isdir(save_directory)
+    #if not check_folder:
+        #os.makedirs(save_directory)
+    
     #d = requests.get(download_url, stream=True)
     
-    #with open('C:/Users/Tyler/Documents'+pretty_directory 'wb') as f :
+    #with open(save_filepath, 'wb') as f :
         #f.write(d.content)
     
     #sleep(2)
 
-#print('sleight of hand check successful')
+#downloadScroll(download_url, scroll_name)
+print('sleight of hand check successful')
